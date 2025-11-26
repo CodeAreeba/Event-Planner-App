64d9da6acd37aeec7365ccca81da703c56e9dab6
CodeAreeba <areebaabdullah111@gmail.com>
2025-11-27 01:40:24 +0500

    fix persistent storage

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,5 +1,6 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { FirebaseApp, initializeApp } from 'firebase/app';
-import { Auth, getAuth } from 'firebase/auth';
+import { Auth, getReactNativePersistence, initializeAuth } from 'firebase/auth';
 import { Firestore, getFirestore } from 'firebase/firestore';
 import { FirebaseStorage, getStorage } from 'firebase/storage';
 
@@ -42,8 +43,10 @@ let storage: FirebaseStorage | null = null;
 try {
     app = initializeApp(firebaseConfig);
 
-    // Initialize Auth
-    auth = getAuth(app);
+    // Initialize Auth with AsyncStorage persistence
+    auth = initializeAuth(app, {
+        persistence: getReactNativePersistence(AsyncStorage)
+    });
 
     db = getFirestore(app);
 
